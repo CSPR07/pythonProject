6de7edfa78511347fd6045af09b71900c49dc58d
Caspar van Vuure <2140608@talnet.nl>
2021-09-06 14:27:59 +0200

    Avontuur spelletje python

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-x = 1
-y = 2
-print(x+y)
+
+antwoord = input("Wil je het spel spelen? (ja/nee) ")
+
+if antwoord.lower().strip() == "ja":
+
+    antwoord = input("Je komt een pad tegen wil je naar links of naar rechts toe? (links/rechts) ").lower().strip()
+    if antwoord == "links":
+        antwoord = input("Boe! je komt een monster tegen. Wat doe je ren je of val je aan? (rennen/aanvallen) ")
+
+        if antwoord == "aanvallen":
+            print("Je probeert het monster aan te vallen maar je gaat dood in het process!")
+        else:
+            print("Goede keuze je bent veilig weggekomen.")
+
+    elif antwoord == "rechts":
+        print("Je gaat naar rechts maar als je een bosje passeert dan val je in een berenkuil en gaat dood!")
+
+    else:
+        print("Error dit kan niet!")
+
+else:
+    print("Dat is jammer :( ")
